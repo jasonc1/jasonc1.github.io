9e2f8046d3dfe1181f02eaf7a7b8e23d29149cef
jchen10 <jason_chen@intuit.com>
2018-04-21 21:49:13 -0700

    added scroll to

--- app/js/script2.js
+++ app/js/script2.js
@@ -15,6 +15,18 @@ $( document ).ready(function() {
 
     $("#about").click(
         function(){
-            $('html, body').animate({ scrollTop: $('#scrollAbout').offset().top -284 }, 'slow');
-        });
+            $('html, body').animate({ scrollTop: $('#scroll-about').offset().top -284 }, 'slow');
+    });
+    $("#projects").click(
+        function(){
+            $('html, body').animate({ scrollTop: $('#scroll-projects').offset().top -284 }, 'slow');
+    });
+    $("#photography").click(
+        function(){
+            $('html, body').animate({ scrollTop: $('#scroll-photography').offset().top -284 }, 'slow');
+    });
+    $("#resume").click(
+        function(){
+            $('html, body').animate({ scrollTop: $('#scroll-resume').offset().top -284 }, 'slow');
+    });
 });
