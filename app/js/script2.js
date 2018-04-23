b764bba3434385bc57c689586a1a9a1d61242b09
jchen10 <jason_chen@intuit.com>
2018-04-22 21:37:33 -0700

    fixed responsiveness and scrollto

--- app/js/script2.js
+++ app/js/script2.js
@@ -1,9 +1,9 @@
 
 $(window).scroll(function(){
-  var distanceFromTop = $(document).scrollTop();    
-  if(distanceFromTop < 1000 )
+  var distanceFromTop = $(document).scrollTop();
+  if(distanceFromTop < 500)
   {
-      $('.header').css('width', '50' - $(document).scrollTop()/10+ '%');
+      $('.header').css('width', '50' - $(document).scrollTop()/20+ '%');
   }
   else
   {
@@ -12,21 +12,21 @@ $(window).scroll(function(){
 });
 
 $( document ).ready(function() {
-
+    var scrollHere = $(".title").position()
     $("#about").click(
         function(){
-            $('html, body').animate({ scrollTop: $('#scroll-about').offset().top -284 }, 'slow');
+            $('html, body').animate({ scrollTop: $('#scroll-about').offset().top - scrollHere.top - 30}, 'slow');
     });
     $("#projects").click(
         function(){
-            $('html, body').animate({ scrollTop: $('#scroll-projects').offset().top -284 }, 'slow');
+            $('html, body').animate({ scrollTop: $('#scroll-projects').offset().top - scrollHere.top - 30}, 'slow');
     });
     $("#photography").click(
         function(){
-            $('html, body').animate({ scrollTop: $('#scroll-photography').offset().top -284 }, 'slow');
+            $('html, body').animate({ scrollTop: $('#scroll-photography').offset().top - scrollHere.top - 30}, 'slow');
     });
     $("#resume").click(
         function(){
-            $('html, body').animate({ scrollTop: $('#scroll-resume').offset().top -284 }, 'slow');
+            $('html, body').animate({ scrollTop: $('#scroll-resume').offset().top - scrollHere.top - 30}, 'slow');
     });
 });
