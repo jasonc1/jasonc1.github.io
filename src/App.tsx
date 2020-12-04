29aca842fab4d5809585d5e8c7e7221eda611bde
Jason Chen <jasonc1@andrew.cmu.edu>
2020-12-04 00:45:25 -0800

    updated responsiveness for prose component, commented out OTM and designsystems at blend blog post

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import { HashRouter, Route, Switch } from 'react-router-dom';
 import { Main } from './pages/Main/Main';
 import { Photography } from './pages/Photography/Photography';
 import UnderConstruction from './pages/UnderConstruction/UnderConstruction.component';
-import OTM from './pages/Projects/OTM';
-import DesignSystemsAtBlend from './pages/Blog/designSystemsAtBlend';
+// import OTM from './pages/Projects/OTM';
+// import DesignSystemsAtBlend from './pages/Blog/designSystemsAtBlend';
 
 export const App = () => {
   return (
@@ -14,8 +14,8 @@ export const App = () => {
           <Route exact path="/" component={Main} />
           <Route path="/photography" component={Photography} />
           <Route path="/underconstruction" component={UnderConstruction} />
-          {/* <Route path="/OTM" component={OTM} />
-          <Route path="/dsystemsAtBlend" component={DesignSystemsAtBlend} /> */}
+          {/* <Route path="/OTM" component={OTM} /> */}
+          {/* <Route path="/dsystemsAtBlend" component={DesignSystemsAtBlend} /> */}
         </Switch>
       </HashRouter>
     </div>
--- src/pages/Photography/Photography.tsx
+++ src/pages/Photography/Photography.tsx
@@ -4,6 +4,10 @@ import { Text } from '../../components/text/text.component';
 import { Footer } from '../../components/footer/footer.component';
 import { theme } from '../../colors.js';
 import pgh from '../../assets/images/pgh.jpg';
+import montanadoro from '../../assets/images/montanadoro.jpg';
+import bodega from '../../assets/images/bodega.jpg';
+import honolulu from '../../assets/images/honolulu.jpg';
+import halfmoon from '../../assets/images/halfmoon.jpg';
 
 const summary = `Here are just some of my favorite photographs that I’ve taken over the years. I shoot primarily 120 film (aka ‘medium format’), but sometimes I still use a Nikon FM2 for good old 35mm film - the stuff most of us grew up on before the digital age. For medium format, I currently own a Bronica ETRSi, Mamiya RZ67, and Plaubel Makina 67.`;
 
@@ -22,8 +26,13 @@ export const Photography = () => {
         text={summary}
         marginBottom={80}
       />
-      <img src={pgh} alt="pittsburgh" />
-      <Footer color={theme.black} marginTop={80} />
+      <img src={pgh} alt="Pittsburgh, Pennsylvania" />
+      <img src={montanadoro} alt="Montaña D'oro, San Luis Obispo, California" />
+      <img src={bodega} alt="Bodega Bay, California" />
+      <img src={honolulu} alt="Honolulu, Hawaii" />
+      <img src={halfmoon} alt="HalfMoon Bay, California" />
+
+      <Footer color={theme.black} />
     </div>
   );
 };
